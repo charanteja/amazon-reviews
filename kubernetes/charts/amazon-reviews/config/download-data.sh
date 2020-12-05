d46241edc1586644199e9e637069e42a8f3efdda
Charan Kattumenu <charan.kattumenu@tradeshift.com>
2020-12-05 21:39:07 +0100

    Update kubernetes helm chart

--- kubernetes/charts/amazon-reviews/config/download-data.sh
+++ kubernetes/charts/amazon-reviews/config/download-data.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# download reviews
-wget http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Amazon_Instant_Video.json.gz
+# download reviews from the location
+wget https://s3-eu-west-1.amazonaws.com/bigdata-team/job-interview/item_dedup.json.gz
 
 # put reviews into hdfs
 ./hadoop-2.9.0/bin/hadoop fs -copyFromLocal reviews_Amazon_Instant_Video.json.gz hdfs://hadoop-hadoop-hdfs-nn:9000/data/reviews/reviews_Amazon_Instant_Video.json.gz
 
-# download metadata
-wget http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/meta_Amazon_Instant_Video.json.gz
+# download metadata from the location
+wget https://s3-eu-west-1.amazonaws.com/bigdata-team/job-interview/metadata.json.gz
 
 # put metadata into hdfs
 ./hadoop-2.9.0/bin/hadoop fs -copyFromLocal meta_Amazon_Instant_Video.json.gz hdfs://hadoop-hadoop-hdfs-nn:9000/data/metadata/meta_Amazon_Instant_Video.json.gz
